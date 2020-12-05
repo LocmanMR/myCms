deac22771eba35b416321709ded9d04e57d1ec7a
Locman <israfilov.ruslan2018@yandex.ru>
2020-12-05 16:22:36 +0300

    created markdownParser and some yaml configs

--- src/Service/ArticleProvider.php
+++ src/Service/ArticleProvider.php
@@ -25,6 +25,14 @@ class ArticleProvider
         ],
     ];
 
+    private $markdownParser;
+
+    public function __construct(MarkdownParser $markdownParser)
+    {
+        $this->markdownParser = $markdownParser;
+    }
+
+
     public function articles(): array
     {
         return self::ARTICLES;
@@ -32,7 +40,20 @@ class ArticleProvider
 
     public function article(): array
     {
-        return self::ARTICLES[array_rand(self::ARTICLES, 1)];
+        $articleContent = <<<EOF
+Lorem ipsum **красная точка** dolor sit amet, consectetur adipiscing elit, sed
+do eiusmod tempor incididunt [Сметанка](/) ut labore et dolore magna aliqua.
+Purus viverra accumsan in nisl. Diam vulputate ut pharetra sit amet aliquam. Faucibus a
+pellentesque sit amet porttitor eget dolor morbi non. Est ultricies integer quis auctor
+elit sed. Tristique nulla aliquet enim tortor at. Tristique et egestas quis ipsum. Consequat semper viverra nam
+libero. Lectus quam id leo in vitae turpis. In eu mi bibendum neque egestas congue
+quisque egestas diam. **Красная точка** blandit turpis cursus in hac habitasse platea dictumst quisque.
+EOF;
+
+        $article = self::ARTICLES[array_rand(self::ARTICLES, 1)];
+        $article['articleContent'] = $this->markdownParser->parse($articleContent);
+
+        return $article;
     }
 
 }
---
+++ src/Service/MarkdownParser.php
@@ -0,0 +1,59 @@
+<?php
+
+
+namespace App\Service;
+
+
+use Demontpx\ParsedownBundle\Parsedown;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
+
+class MarkdownParser
+{
+    /**
+     * @var Parsedown
+     */
+    private $parsedown;
+    /**
+     * @var AdapterInterface
+     */
+    private $cache;
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+    /**
+     * @var bool
+     */
+    private $debug;
+
+    public function __construct(
+        Parsedown $parsedown,
+        AdapterInterface $cache,
+        LoggerInterface $markdownLogger,
+        bool $debug
+    ) {
+        $this->parsedown = $parsedown;
+        $this->cache = $cache;
+        $this->logger = $markdownLogger;
+        $this->debug = $debug;
+    }
+
+    public function parse(string $source): string
+    {
+        if (empty($source)) {
+            $this->logger->info('Article does not contain content');
+        }
+
+        if ($this->debug) {
+            return $this->parsedown->text($source);
+        }
+
+        return $this->cache->get(
+            'markdown_'.md5($source),
+            function () use ($source) {
+                return $this->parsedown->text($source);
+            }
+        );
+    }
+}
